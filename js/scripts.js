58785812bc9264157ce8deb19dc3b3a906040a2f
ozinadia <88419005+ozinadia@users.noreply.github.com>
2021-08-06 17:03:33 +0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+
+jQuery(document).ready(function() {
+
+    /*
+        Background slideshow
+    */
+    $('#banner').backstretch([
+      "new/IMG_9635.jpg"
+    , "new/IMG_0198.jpg"
+    , "new/IMG_0202.jpg"
+    , "new/IMG_9623.jpg"
+    ], {duration: 3000, fade: 750});
+});
